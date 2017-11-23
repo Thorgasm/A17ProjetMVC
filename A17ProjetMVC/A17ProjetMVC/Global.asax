eafaea975255f877d58dbeb329cf6080f1360440
1395118 <1395118@LLBD0622INFO08>
2017-11-23 13:25:27 -0500

    Projet

---
+++ A17ProjetMVC/A17ProjetMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="A17ProjetMVC.MvcApplication" Language="C#" %>
---
+++ A17ProjetMVC/A17ProjetMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(A17ProjetMVC.Startup))]
+namespace A17ProjetMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
