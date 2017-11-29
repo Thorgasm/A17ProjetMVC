cf138099d883c2814d22b42b18f38aa2d0a4cd1b
louisenault <louis@triod.ca>
2017-11-29 09:04:16 -0500

    EmpruntVM

---
+++ A17ProjetMVC/A17ProjetMVC/Models/EmpruntVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace A17ProjetMVC.Models
+{
+    public class EmpruntVM
+    {
+
+        public int ObjetID;
+
+        public int nbJours;
+
+    }
+}
