3fcaa52244d6e56c51518e7b4171f38dfdee142a
1551579 <1551579@LLBD0622INFO09>
2017-12-07 10:57:06 -0500

    Tests Proj

---
+++ A17ProjetMVC/A17ProjetMVC_Tests/TestsControllers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace A17ProjetMVC_Tests
+{
+    [TestClass]
+    public class TestsControllers
+    {
+        [TestMethod]
+        public void Test1()
+        {
+        }
+    }
+}
